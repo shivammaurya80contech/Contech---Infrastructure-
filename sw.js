cd93ed419b28ccc698902fcb17f19f9cb6e2c51a
shivammaurya80contech <shivammaurya7182@gmail.com>
2026-06-25 22:24:41 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,25 @@
+const CACHE_NAME = "contech-v1";
 
 const urlsToCache = [
-  "/shivammaurya80contech/",
-  "/shivammaurya80contech/index.html",
-  "/shivammaurya80contech/manifest.json",
-  "/shivammaurya80contech/icon-192.png",
-  "/shivammaurya80contech/icon-512.png"
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./Shivam.jpg",
+  "./building.jpg"
 ];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
